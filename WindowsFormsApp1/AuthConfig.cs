1aedf32794c807cdb5ba55ac494cf2d5f96d1e2d
Sean Davitt <sean.davitt@tylertech.com>
2021-05-27 20:00:07 -0400

    change of namespace

--- WindowsFormsApp1/AuthConfig.cs
+++ WindowsFormsApp1/AuthConfig.cs
@@ -2,8 +2,25 @@
 using System;
 using System.Globalization;
 using System.IO;
+using Microsoft.Identity.Client;
+using MobileInstallApp;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Management;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Security.AccessControl;
+using System.ServiceProcess;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
 
-namespace AppPubDeployUtility
+namespace Mobile_App
 {
     public class AuthConfig
     {
